8a5c8aa24d169f9c77169d171bd80b93d7165e72
sarbada <sarbadaj@yahoo.com>
2014-08-10 17:18:26 +0545

    added usermailer in action send password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,7 @@ class User < ActiveRecord::Base
     generate_token(:password_reset_token)
     self.password_reset_sent_at = Time.zone.now
     save!
+    UserMailer.password_reset(self).deliver
   end
   
   def generate_token(column)
