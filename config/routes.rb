f9b41ef2a4a188be212371d8005abf85a10e4421
sarbada <sarbadaj@yahoo.com>
2014-08-10 16:23:26 +0545

    generated password resets controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Todo::Application.routes.draw do
+  get 'password_resets/new'
+
   get 'sessions/new'
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
