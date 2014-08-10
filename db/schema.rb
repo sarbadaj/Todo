446d33b5ba23f5a540fb6b54c87a2989789479e3
sarbada <sarbadaj@yahoo.com>
2014-08-10 16:53:05 +0545

    generated migration add password reset to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140810070059) do
+ActiveRecord::Schema.define(version: 20140810110619) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20140810070059) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "auth_token"
+    t.string   "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
 end
