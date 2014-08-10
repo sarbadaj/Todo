9f1ab1d87ec6f2ad98eab9cf7879e960f3f8c727
sarbada <sarbadaj@yahoo.com>
2014-08-10 17:04:55 +0545

    created action send password reset and created method generate token for password reset token

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,9 +27,16 @@ class User < ActiveRecord::Base
     end
   end
 
+  def send_password_reset
+    generate_token(:password_reset_token)
+    self.password_reset_sent_at = Time.zone.now
+    save!
+  end
+  
   def generate_token(column)
     begin
        self[column] = SecureRandom.urlsafe_base64
      end while User.exists?(column => self[column])
   end
+
 end
