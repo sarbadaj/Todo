abfbd5ec99dfa23ed417d8b69f73d7937fdf4833
sarbada <sarbadaj@yahoo.com>
2014-08-12 23:53:32 +0545

    these modification have been already commited

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811013525) do
+ActiveRecord::Schema.define(version: 20140812163951) do
 
   create_table "projects", force: true do |t|
     t.string   "title"
     t.text     "body"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "projects", ["user_id"], name: "index_projects_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
