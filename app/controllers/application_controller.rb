341334bf5add32ae21a813957eb0badae58397b2
sarbada <sarbadaj@yahoo.com>
2014-08-12 15:55:49 +0545

    modified for remember me action and for porject table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
 	private
-
-	def current_user
-	  @current_user ||= User.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
-	end
+		def current_user
+		  @current_user ||= User.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
+		end
 end
--- db/migrate/20140812091848_create_projects.rb
+++ db/migrate/20140812091848_create_projects.rb
@@ -3,6 +3,7 @@ class CreateProjects < ActiveRecord::Migration
     create_table :projects do |t|
       t.string :title
       t.text :body
+      t.references :user, index: true
 
       t.timestamps
     end
