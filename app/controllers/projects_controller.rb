e9f892fe511c74affdf6c6f6a5eefea6bb3f67f6
sarbada <sarbadaj@yahoo.com>
2014-08-12 21:50:06 +0545

    created action for add project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,18 @@
 class ProjectsController < ApplicationController
   def new
+  	@project = Project.new
+  	@user = current_user
   end
+
+  def create
+    @user = User.find(params[:user_id])
+    @project = @user.projects.create(project_params)
+    redirect_to users_path
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(:title, :body)
+    end
+
 end
