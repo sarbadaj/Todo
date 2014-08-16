1f6ba8ff6bb97fb4e2a687d10824d6d9e9dbddf2
sarbada <sarbadaj@yahoo.com>
2014-08-16 07:49:42 +0545

    migrated for task

---
+++ db/migrate/20140814182921_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :body
+      t.string :member
+      t.references :project, index: true
+
+      t.timestamps
+    end
+  end
+end
