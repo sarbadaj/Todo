b4d685510758321a4243fedcf65c57a7cc6a2993
sarbada <sarbadaj@yahoo.com>
2014-08-16 08:21:14 +0545

    these are generated while generated task model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140814120321) do
+ActiveRecord::Schema.define(version: 20140814191353) do
+
+  create_table "members", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "projects", force: true do |t|
     t.string   "title"
@@ -27,9 +34,12 @@ ActiveRecord::Schema.define(version: 20140814120321) do
     t.string   "title"
     t.text     "body"
     t.string   "member"
+    t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "tasks", ["project_id"], name: "index_tasks_on_project_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
