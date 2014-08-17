0e6a8d73da6c4d65866e68b423085574afdd957d
sarbada <sarbadaj@yahoo.com>
2014-08-17 18:10:31 +0545

    modified path for move after complete sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		redirect_to users_path, :notice => "Signed up"
+  		redirect_to log_in_path, :notice => "Signed up"
   	else
   	render "new"
   	end	
