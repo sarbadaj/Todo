dea0ebd5cc0063d021b4538bd3cffcb22cd8bdf2
sarbada <sarbadaj@yahoo.com>
2014-08-17 18:06:46 +0545

    modified create, index and new action and added destroy action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,32 @@
 class TasksController < ApplicationController
   
   def new
+    @project = Project.find(params[:project_id])
   	@task = Task.new
   end
 
  	def create
     @project = Project.find(params[:project_id])
     @task = @project.tasks.create(task_params)
-    redirect_to tasks_path
+    redirect_to project_tasks_path(@project)
+    TaskInfoMailer.task_info(@task).deliver
   end
 
   def index
+    @project = Project.find(params[:project_id])
   	@tasks = Task.new
   end
+
   def show
     @task = Task.find(params[:id])  
   end
 
+  def destroy
+    @project = Project.find(params[:project_id])
+    @task = @project.tasks.find(params[:id])
+    @task.destroy
+    redirect_to project_tasks_path(@project)
+  end
   private
     def task_params
       params.require(:task).permit(:title, :body, :member)
