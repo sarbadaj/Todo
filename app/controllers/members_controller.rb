802c200b9ee10a2d634df57a91f11d16a8470117
sarbada <sarbadaj@yahoo.com>
2014-08-17 18:05:03 +0545

    modified create, index and new action and added destroy action

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,21 +1,29 @@
 class MembersController < ApplicationController
   def new
+  	@project = Project.find(params[:project_id])
   	@member = Member.new
   end
   
   def create
-  	@member = Member.new(member_params)
-  	
-  	@member.save
- 		render 'tasks/index'
+  	@project = Project.find(params[:project_id])
+    @member = @project.members.create(member_params)
+    redirect_to project_tasks_path(@project)
   end
 
   def index
+  	@project = Project.find(params[:project_id])
   	@members = Member.all
   end
 
   def show
   	@member = Member.find(params[:id])
+  end
+
+  def destroy
+  	@project = Project.find(params[:project_id])
+  	@member = @project.members.find(params[:id])
+  	@member.destroy
+  	redirect_to project_tasks_path(@project)
   end
 
   private
